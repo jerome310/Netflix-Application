9a90a194a83de6dab5c2ce02c330c1379b1885f0
jerome310 <jeromeyoung333@gmail.com>
2023-10-11 07:56:47 -0400

    add audio functionality to mute button within desktop button

--- movie.js
+++ movie.js
@@ -1,7 +1,6 @@
 // Volume Button Feature
 const muteBtn = document.querySelector(".mute");
 const playBtn = document.querySelector(".play");
-//const video = document.querySelector("#video source").src;
 const video = document.querySelector("#video");
 
 // This function mute's the video audio
@@ -25,6 +24,47 @@ playBtn.addEventListener("click", () => {
   playBtn.style.display = "none";
   muteBtn.style.display = "block";
   toggleMuted();
+});
+
+// Desktop Audio Button
+const playButton = document.querySelector(".play-button");
+console.log(playButton)
+const muteAudio = document.querySelector(".mute-audio-img");
+const playAudio = document.querySelector(".play-audio-img");
+//const muteButton = document.querySelector(".mute-button");
+const desktopVideo = document.querySelector("#video");
+
+// This function mute's the video audio
+function toggleMuted() {
+  desktopVideo.muted = desktopVideo;
+}
+
+// This function unmute's the video audio
+function toggleunMuted() {
+  desktopVideo.muted = !desktopVideo;
+}
+
+// Play & Mute Button Feature
+// muteBtn.addEventListener("click", () => {
+//   playBtn.style.display = "block";
+//   muteBtn.style.display = "none";
+//   toggleunMuted();
+// });
+
+playButton.addEventListener("click", () => {
+  if (muteAudio.style.display = 'block') {
+    muteAudio.style.display = "none";
+    playAudio.style.display = "block";
+    toggleunMuted();
+  } else {
+    
+  }
+  // if (playAudio.style.display = "none") {
+  //   playAudio.style.display = "block";
+  //   toggleMuted();
+  // }
+  //playButton.style.display = "none";
+  //muteBtn.style.display = "block";
 });
 
 // Movies
